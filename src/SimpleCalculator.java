6bc69005a6c0e0f178ad97c2ca8d8ee10c7ded43
adamd <adam.d.hinton@gmail.com>
2024-02-20 17:06:47 -0500

    fields and get/setters

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,3 +1,23 @@
 public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+
 
 }
